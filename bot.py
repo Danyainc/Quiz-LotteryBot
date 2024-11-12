6bd1b1f922a168a608aa70e259c7bd708a9530e4
Danyanic <danila19032@gmail.com>
2024-11-12 13:47:38 +0300

    Добавление работы с розыгрышами

--- bot.py
+++ bot.py
@@ -1,5 +1,9 @@
 import os
-import logging
+from datetime import datetime
+from random import choice
+
+import re
+
 from db import *
 from dotenv import load_dotenv
 import telebot
@@ -15,29 +19,30 @@ logger = telebot.logger
 
 main_menu_button = KeyboardButton("Вернуться в главное меню")
 channels_button = KeyboardButton("Управление каналами", )
-quizzes_button = KeyboardButton("Управление викторинами",
+quizzes_button = KeyboardButton("Создать и отправить викторину",
                                 request_poll=KeyboardButtonPollType('quiz'))
-lotteries_button = KeyboardButton("Управление розыгрышами")
 
-add_quiz_button = InlineKeyboardButton("Начать чат",
-                                       callback_data='add_group')
-add_lottery_button = InlineKeyboardButton("Начать чат",
-                                          callback_data='add_group')
-del_lottery_button = InlineKeyboardButton("Начать чат",
-                                          callback_data='add_group')
-del_quiz_button = InlineKeyboardButton("Начать чат",
-                                       callback_data='add_group')
-channel_list = KeyboardButton('Список добавленных каналов')
+lotteries_button = KeyboardButton("Управление розыгрышами")
+add_lotteries_button = KeyboardButton('Добавить розыгрыш')
+list_lotteries_button = KeyboardButton('Список розыгрышей')
+
+channel_list_button = KeyboardButton('Список добавленных каналов')
+
 add_channel_button = KeyboardButton("Добавить канал",
                                     request_chat=KeyboardButtonRequestChat(1, True))
-
+back_to_lottery_list_button = InlineKeyboardButton('К списку розыгрышей', callback_data='back_to_lottery_list')
 main_keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(quizzes_button, channels_button, lotteries_button, )
 
-channels_keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(add_channel_button, channel_list).add(main_menu_button)
+channels_keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(add_channel_button, channel_list_button).add(
+    main_menu_button)
+lotteries_keyboard = ReplyKeyboardMarkup(resize_keyboard=True).add(add_lotteries_button, list_lotteries_button).add(
+    main_menu_button
+)
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    print(message)
     if not is_user(message.from_user.id):
         add_user(message.from_user.id, message.from_user.username)
     bot.send_message(message.chat.id,
@@ -48,7 +53,6 @@ def start(message):
 
 @bot.message_handler(content_types=['poll'])
 def add_poll(message):
-    print(message)
     question = message.poll.question
     options = message.poll.options
     correct_option_id = message.poll.correct_option_id
@@ -60,16 +64,6 @@ def add_poll(message):
                   explanation=explanation)
 
 
-@bot.message_handler(commands=['start'])
-def start(message):
-    if not is_user(message.from_user.id):
-        add_user(message.from_user.id, message.from_user.username)
-    bot.send_message(message.chat.id,
-                     text="Добро пожаловать!",
-                     reply_markup=main_keyboard
-                     )
-
-
 @bot.message_handler(func=lambda message: message.text == channels_button.text)
 def channels_menu(message):
     bot.send_message(message.chat.id, 'Меню управления каналами, можно добавить канал или просмотреть уже существующие',
@@ -77,49 +71,209 @@ def channels_menu(message):
 
 
 @bot.message_handler(func=lambda message: message.text == main_menu_button.text)
-def channels_menu(message):
+def back_to_main_menu(message):
     bot.send_message(message.chat.id, 'главное меню',
                      reply_markup=main_keyboard)
 
 
-@bot.message_handler(func=lambda message: message.text == channel_list.text)
-def channels_list(message, chat=None):
+@bot.message_handler(func=lambda message: message.text == lotteries_button.text)
+def lotteries_menu(message):
+    bot.send_message(message.chat.id, 'Управление розыгрыщами',
+                     reply_markup=lotteries_keyboard)
+
+
+@bot.message_handler(func=lambda message: message.text == add_lotteries_button.text)
+def add_lottery(message):
+    bot.send_message(message.chat.id,
+                     'Добавление розыгрыша. Также укажите дату окончания розыгрыша DD.MM.YYYY HH:MM')
+    bot.register_next_step_handler(message, process_lottery_info)
+
+
+def process_lottery_info(message):
+    try:
+        text = message.text
+        date_pattern = r'(d{2}.d{2}.d{4}(?: d{2}:d{2})?)'
+        end_time = re.search(date_pattern, text).group(0)
+        if len(end_time) > 10:
+            end_time = datetime.strptime(end_time, '%d.%m.%Y %H:%M').timestamp()
+        else:
+            end_time = datetime.strptime(end_time, '%d.%m.%Y').timestamp()
+        add_lottery_to_db(text, message.from_user.id, end_time)
+        lottery_id = get_lotteries(message.from_user.id)[-1][0]
+        send_button = InlineKeyboardButton('Отправить в канал', callback_data=f'send {lottery_id}')
+        lottery_action_keyboard = InlineKeyboardMarkup().add(send_button)
+        bot.send_message(message.chat.id, 'Розыгрш создан!', reply_markup=lottery_action_keyboard)
+    except Exception as e:
+        print(e)
+
+
+@bot.message_handler(func=lambda message: message.text == list_lotteries_button.text)
+def list_lottery(message, chat=None):
+    lotteries = get_lotteries(message.from_user.id) if not chat else get_lotteries(chat.id)
+    inline_keyboard = InlineKeyboardMarkup()
+    for lottery in lotteries:
+        inline_keyboard.add(InlineKeyboardButton(f'{lottery[1][:50]}...', callback_data=f'get_lottery {lottery[0]}'))
+    if not chat:
+        bot.send_message(message.chat.id, 'Список созданных розыгрышей',
+                         reply_markup=inline_keyboard)
+    else:
+        bot.send_message(chat.id, 'Список созданных розыгрышей',
+                         reply_markup=inline_keyboard)
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data.startswith('back_to_lottery_list'))
+def back_to_lottery_list(callback):
+    bot.delete_message(callback.message.chat.id, callback.message.id)
+    list_lottery(callback.message, callback.message.chat)
+
+
+def get_channel_list_keyboard(message, type_callback, chat=None, item=None):
     channels = get_channels(message.from_user.id) if not chat else get_channels(chat.id)
+    item_id = item if item else None
     channels_list_inline_markup = InlineKeyboardMarkup()
     for channel_id in channels:
         channel = bot.get_chat(channel_id[0])
-        button = InlineKeyboardButton(channel.username, callback_data=f'channel {channel.id}')
+        callback_data = f'{type_callback} channel {channel.id} {item_id}' if item else f'{type_callback} channel {channel.id}'
+        button = InlineKeyboardButton(channel.username, callback_data=callback_data)
         channels_list_inline_markup.add(button)
+    return channels_list_inline_markup
+
+
+def get_winner_in_lottery(lottery_id):
+    users = get_users_in_lottery(lottery_id)
+    if users:
+        winner = choice(users)
+        return winner
+
+
+@bot.message_handler(func=lambda message: message.text == channel_list_button.text)
+def channels_list(message):
     bot.send_message(message.chat.id,
                      'Добавленные каналы, если канала нету в списке, убедитесь, что бот добавлен в канал : ',
-                     reply_markup=channels_list_inline_markup)
+                     reply_markup=get_channel_list_keyboard(message, 'list'))
 
 
-@bot.callback_query_handler(func=lambda callback: True)
-def check_callback(callback):
-    if callback.data.startswith('channel'):
-        channel = callback.data.split()[1]
-        inline_keyboard = InlineKeyboardMarkup()
-        back_to_channel_list_inline_button = InlineKeyboardButton('Назад к списку каналов',
-                                                                  callback_data='back_to_channel_list')
-        del_channel_inline_button = InlineKeyboardButton('Удалить канал из списка',
-                                                         callback_data=f'del_channel {channel}')
-        inline_keyboard.add(back_to_channel_list_inline_button, del_channel_inline_button)
-        bot.edit_message_text(chat_id=callback.message.chat.id,
-                              message_id=callback.message.id,
-                              text=f'Был выбран {channel} channel',
-                              reply_markup=inline_keyboard)
-    if callback.data.startswith('del_channel'):
-        del_channel(int(callback.data.split()[1]))
-        bot.delete_message(callback.message.chat.id, callback.message.id)
-        channels_list(callback.message)
-    if callback.data.startswith('back_to_channel_list'):
-        bot.delete_message(callback.message.chat.id, callback.message.id)
-        channels_list(callback.message, callback.message.chat)
+@bot.callback_query_handler(func=lambda callback: callback.data.startswith('end_lottery'))
+def end_lottery_callback(callback):
+    lottery_id = int(callback.data.split()[1])
+    lottery = get_lottery(lottery_id)
+    if lottery[3] <= datetime.now().timestamp() and lottery[4]:
+        channels = get_channels_with_lottery(lottery_id)
+        winner = get_winner_in_lottery(lottery_id)
+        if winner and channels:
+            username = get_user(winner[0])
+            for channel in channels:
+                bot.send_message(channel[0], f'Победитель розыгрыша @{username[1]}')
+        else:
+            bot.answer_callback_query(callback.id, "К сожалению никто не принял участи в розыгрше")
+        change_lottery_status(lottery_id, 0)
+
+    else:
+        bot.answer_callback_query(callback.id, "Розыгрыш ещё не окончен")
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data.startswith('list channel'))
+def send_channel(callback):
+    channel = callback.data.split()[2]
+    inline_keyboard = InlineKeyboardMarkup()
+    back_to_channel_list_inline_button = InlineKeyboardButton('Назад к списку каналов',
+                                                              callback_data='back_to_channel_list')
+    del_channel_inline_button = InlineKeyboardButton('Удалить канал из списка',
+                                                     callback_data=f'del_channel {channel}')
+    inline_keyboard.add(back_to_channel_list_inline_button, del_channel_inline_button)
+    bot.edit_message_text(chat_id=callback.message.chat.id,
+                          message_id=callback.message.id,
+                          text=f'Был выбран {channel} channel',
+                          reply_markup=inline_keyboard)
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data.startswith('back_to_channel_list'))
+def back_to_channel_list(callback):
+    bot.delete_message(callback.message.chat.id, callback.message.id)
+    bot.send_message(callback.message.chat.id,
+                     'Добавленные каналы, если канала нету в списке, убедитесь, что бот добавлен в канал : ',
+                     reply_markup=get_channel_list_keyboard(callback.message, 'list', chat=callback.message.chat))
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data.startswith('del_channel'))
+def delete_channel_from_bot(callback):
+    del_channel(int(callback.data.split()[1]))
+    bot.delete_message(callback.message.chat.id, callback.message.id)
+    bot.answer_callback_query(callback.id, "Вы успешно удалили канал из списка!")
+    channels_list(callback.message)
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data.startswith('del_lottery'))
+def delete_channel_from_bot(callback):
+    lottery_id = int(callback.data.split()[1])
+    lottery = get_lottery(lottery_id)
+    if lottery[4]:
+        bot.answer_callback_query(callback.id, "Нельзя удалить активный розыгрыш!")
+    else:
+        delete_lottery(lottery_id)
+        bot.answer_callback_query(callback.id, "розыгрыш успешно удален!")
+    back_to_lottery_list(callback)
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data.startswith('get_lottery'))
+def send_lottery_in_select_channel(callback):
+    lottery_id = int(callback.data.split()[1])
+    bot.delete_message(callback.message.chat.id, callback.message.id)
+    lottery = get_lottery(lottery_id)
+    end_time = datetime.fromtimestamp(lottery[3])
+    send_button = InlineKeyboardButton('Отправить в канал', callback_data=f'send {lottery_id}')
+    del_lottery_button = InlineKeyboardButton('Удалить розыгрыш', callback_data=f'del_lottery {lottery_id}')
+    end_lottery_button = InlineKeyboardButton('Подвести итоги розыгрыша', callback_data=f'end_lottery {lottery_id}')
+    lottery_action_keyboard = InlineKeyboardMarkup().add(send_button, end_lottery_button).add(
+        back_to_lottery_list_button, del_lottery_button)
+    bot.send_message(callback.message.chat.id, text=f'{lottery[1]} Дата окончания {end_time}',
+                     reply_markup=lottery_action_keyboard)
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data.startswith('send channel'))
+def send_lottery_in_select_channel(callback):
+    lottery_id = int(callback.data.split()[3])
+    channel_id = int(callback.data.split()[2])
+    if not check_lottery_in_channel(lottery_id, channel_id):
+        callback_data = f'join {lottery_id}'
+        participants_button = InlineKeyboardButton('Участвовать', callback_data=callback_data)
+        participants_keyboad = InlineKeyboardMarkup().add(participants_button)
+        bot.send_message(channel_id, callback.message.text, reply_markup=participants_keyboad)
+        change_lottery_status(lottery_id, 1)
+        add_lottery_to_channel(lottery_id, channel_id)
+    else:
+        bot.answer_callback_query(callback.id, "Вы уже отправили этот розыгрыш в канал")
+    back_to_lottery_list(callback)
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data.startswith('send'))
+def send_item_to_channel(callback):
+    keyboard = get_channel_list_keyboard(callback.message, 'send',
+                                         chat=callback.message.chat,
+                                         item=callback.data.split()[1])
+    bot.edit_message_reply_markup(callback.message.chat.id, callback.message.id, reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda callback: callback.data.startswith('join'))
+def callback_join_to_lottery(callback):
+    user_id = callback.from_user.id
+    lottery_id = callback.data.split()[1]
+    lottery = get_lottery(lottery_id)
+    lottery_status = lottery[4]
+    lottery_end_time = lottery[3]
+    print(lottery)
+    if not check_user_in_lottery(lottery_id, user_id) and \
+            lottery_end_time >= datetime.now().timestamp() and lottery_status:
+        join_user_to_lottery(user_id, lottery_id)
+        bot.answer_callback_query(callback.id, "Вы успешно участвуете в розыгрыше!")
+    elif lottery_end_time < datetime.now().timestamp():
+        bot.answer_callback_query(callback.id, "Розыгрыш уже закончен!")
+    else:
+        bot.answer_callback_query(callback.id, "Вы уже участвуете в розыгрыше!")
 
 
 @bot.message_handler(commands=['help'])
-def help_nessage(message):
+def help_message(message):
     bot.send_message(message.chat.id, 'Help Text')
 
 
--- db.py
+++ db.py
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS Users (
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Quizzes (
     id INTEGER PRIMARY KEY,
-    author_quiz INTEGER REFERENCES Users(telegram_id) ON UPDATE CASCADE,
+    author_quiz INTEGER REFERENCES Users(telegram_id) ON DELETE CASCADE,
     questions_id TEXT NOT NULL,
     date_end DATETIME NOT NULL
 )
@@ -20,83 +20,164 @@ CREATE TABLE IF NOT EXISTS Quizzes (
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Lotteries (
     id INTEGER PRIMARY KEY,
-    author_lot INTEGER REFERENCES Users(telegram_id) ON UPDATE CASCADE,
-    date_end_of_lot DATETIME NOT NULL
+    context_lottery TEXT,
+    author_lot INTEGER REFERENCES Users(telegram_id) ON DELETE CASCADE,
+    date_end_of_lot FLOAT NOT NULL,
+    is_active BOOLEAN DEFAULT(FALSE)
+)
+''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS UsersLotteries (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER REFERENCES Users(telegram_id) ON DELETE CASCADE,
+    lottery_id INTEGER REFERENCES Lotteries(id) ON DELETE CASCADE
 )
 ''')
 cursor.execute('''
 CREATE TABLE IF NOT EXISTS Channels (
     id INTEGER PRIMARY KEY,
-    channel_admin INTEGER REFERENCES Users(telegram_id) ON UPDATE CASCADE
+    channel_admin INTEGER REFERENCES Users(telegram_id) ON DELETE CASCADE
+)
+''')
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS ChannelsLotteries (
+    id INTEGER PRIMARY KEY,
+    channel_id INTEGER REFERENCES Channels(id) ON DELETE CASCADE,
+    lottery_id INTEGER REFERENCES Lotteries(id) ON DELETE CASCADE
+
 )
 ''')
 
 connection.commit()
 
 
-def add_user(tg_id, username):
+def execute_query(query, params=()):
     with connection as con:
         cur = con.cursor()
-        cur.execute('''
+        try:
+            cur.execute(query, params)
+            con.commit()
+            return cur
+        except Exception as e:
+            print(f"Error executing query: {e}")
+            return None
+
+
+def add_user(tg_id, username):
+    execute_query('''
         INSERT INTO Users (telegram_id, username) VALUES (?, ?)
         ''', (tg_id, username))
-        con.commit()
-        print('all_good')
 
 
 def is_user(tg_id):
-    with connection as con:
-        cur = con.cursor()
-        cur.execute(f'''
+    result = execute_query(f'''
         SELECT COUNT(*)  FROM Users WHERE telegram_id = ?
         ''', (tg_id,))
-        count = cur.fetchone()[0]
-        return count > 0
+    return result.fetchone()[0] > 0
 
 
 def get_user(tg_id):
-    with connection as con:
-        cur = con.cursor()
-        cur.execute(f'''
-        SELECT *  FROM Users WHERE telegram_id = ?
-        ''', (tg_id,))
-        return cur.fetchone()[0]
+    result = execute_query(f'''
+    SELECT *  FROM Users WHERE telegram_id = ?
+    ''', (tg_id,))
+    return result.fetchone()
 
 
 def add_channel(channel_id, tg_id):
     if is_user(tg_id):
-        with connection as con:
-            user_id = get_user(tg_id)
-            cur = con.cursor()
-            cur.execute(f'''
-                    INSERT INTO Channels (id, channel_admin) VALUES (?, ?)
-            ''', (channel_id, user_id))
-            print('good')
-            con.commit()
+        user_id = get_user(tg_id)[0]
+        execute_query(f'''
+                INSERT INTO Channels (id, channel_admin) VALUES (?, ?)
+        ''', (channel_id, user_id))
 
 
 def get_channel(channel_id):
-    with connection as con:
-        cur = con.cursor()
-        cur.execute(f'''
-        SELECT *  FROM Channels WHERE id = ?
-        ''', (channel_id,))
-        return cur.fetchone()
+    result = execute_query(f'''
+    SELECT *  FROM Channels WHERE id = ?
+    ''', (channel_id,))
+    return result.fetchone()
 
 
 def get_channels(tg_id):
-    with connection as con:
-        cur = con.cursor()
-        cur.execute(f'''
+    result = execute_query(f'''
         SELECT id  FROM Channels WHERE channel_admin = ?
         ''', (tg_id,))
-        return cur.fetchall()
+    return result.fetchall()
 
 
 def del_channel(channel_id):
-    with connection as con:
-        cur = con.cursor()
-        cur.execute(f'''
+    execute_query(f'''
            DELETE  FROM Channels WHERE id = ?
            ''', (channel_id,))
-        return cur.fetchall()
+
+
+def add_lottery_to_db(text, author_id, date_end):
+    execute_query(f'''
+                INSERT INTO Lotteries (context_lottery, author_lot, date_end_of_lot) VALUES (?, ?, ?)
+        ''', (text, author_id, date_end))
+
+
+def get_lotteries(author_id):
+    result = execute_query(f'''
+        SELECT *  FROM Lotteries WHERE author_lot = ?
+        ''', (author_id,))
+    return result.fetchall()
+
+
+def get_lottery(lottery_id):
+    result = execute_query(f'''
+        SELECT *  FROM Lotteries WHERE id = ?
+        ''', (lottery_id,))
+    return result.fetchone()
+
+
+def join_user_to_lottery(user_id, lottery_id):
+    execute_query(f'''
+        INSERT INTO UsersLotteries (user_id, lottery_id) VALUES (?, ?)''',
+                  (user_id, lottery_id))
+
+
+def add_lottery_to_channel(lottery_id, channel_id):
+    execute_query(f'''
+            INSERT INTO ChannelsLotteries (channel_id, lottery_id) VALUES (?, ?)''',
+                  (channel_id, lottery_id))
+
+
+def check_lottery_in_channel(lottery_id, channel_id):
+    result = execute_query(f'''
+            SELECT COUNT(*) FROM ChannelsLotteries WHERE channel_id = ? AND lottery_id = ?''',
+                           (channel_id, lottery_id))
+    return result.fetchone()[0] > 0
+
+
+def check_user_in_lottery(lottery_id, user_id):
+    result = execute_query(f'''
+            SELECT COUNT(*) FROM UsersLotteries WHERE user_id = ? AND lottery_id = ?''',
+                           (user_id, lottery_id))
+    return result.fetchone()[0] > 0
+
+
+def get_channels_with_lottery(lottery_id):
+    result = execute_query(f'''
+            SELECT channel_id FROM ChannelsLotteries WHERE lottery_id = ?''', (lottery_id,)
+                           )
+    return result.fetchall()
+
+
+def get_users_in_lottery(lottery_id):
+    result = execute_query(f'''
+                SELECT user_id FROM UsersLotteries WHERE lottery_id = ?''', (lottery_id,)
+                           )
+    return result.fetchall()
+
+
+def delete_lottery(lottery_id):
+    execute_query(f'''
+                    DELETE FROM Lotteries WHERE id = ?''', (lottery_id,)
+                           )
+
+
+def change_lottery_status(lottery_id, status):
+    execute_query(f'''
+                UPDATE Lotteries SET is_active=? WHERE id=?''',
+                  (status, lottery_id))
